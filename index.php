eb04985a5d58de939fffde9d3579d05350eef011
ippeikato <ippeikato1@gmail.com>
2014-04-29 22:46:15 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,20 +5,26 @@ function h($s) {
 	return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
 }
 
+$posts = array();
+
 try {
 	$dbh = new PDO(DSN, DB_USER, DB_PASSWORD);
 	if ($_SERVER['REQUEST_METHOD'] == 'POST' &&
 	isset($_POST['content']) &&
-	isset($_POST['name'])) 
-	// ここから下が謎です。
-	/*
-	$sql = "select * from posts";
-	$stmt = $dbh->query($sql);
-	*/
+	isset($_POST['name'])) {
+	$stmt = $dbh->query('select * from posts');
+	foreach($stmt as $row) {
+		array_push($posts, $row);
+	}
+	} 
+
 } catch (PDOException $e) {
 	echo $e->getmessage();
 		exit;
 }
+
+$posts = array_reverse($posts);
+
 ?>
 <!DOCTYPE html>
 <hrml lang="ja">
@@ -33,9 +39,15 @@ try {
 		<textarea name="content" rows="8" cols="40"></textarea><br>
 		<input type="submit" value="投稿">
 	</form
-	<h2> 投稿一覧 0件</h2>
+	<h2> 投稿一覧 <?php echo count($posts); ?>件</h2>
 	<ul>
+		<?php if (count($posts)) : ?>
+		<?php foreach ($posts as $post) : ?>
+    		<li><?php echo h($post['name']), h($post['content']); ?></li>
+		<?php endforeach; ?>
+		<?php else : ?>
 			<li>まだ投稿はありません。</li>
+		<?php endif; ?>
 	</ul>
 </body>
 </html>
